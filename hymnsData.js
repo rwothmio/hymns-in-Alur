4a2c4454469dbcf279391281f8db0c6de8ba7373
rwothmio <brwothmio@gmail.com>
2023-08-27 23:52:49 +0300

    initial commit

---
+++ hymnsData.js
@@ -0,0 +1,330 @@
+const hymns = [
+  {
+    number: 1,
+    key: "G",
+    tempo: `4/4`,
+    collections: ["BA 1", "SW 1"],
+    titles: ["Pak Mungu, won gin", "Doxology"],
+    body: [
+      [
+        `Pak Mungu, won gin ma ber kpo,
+ Pake wu dhanu yu mi ng'om.
+Jumalaika pake malu, pak Wego, Wod, Tipo Maleng'.`,
+      ],
+      [
+        `Wu dhanu ma wuyie kpo, mond i ot pare ku pake.
+Wubin bang'e ku mutoro, wuyung Mungu wupak nyinge.`,
+      ],
+      [
+        `Rwoth Yesu E Mungu mandha, Ecwiowa ku tegone.
+Tek wayie, Ejolowa, Ekwayuwa ni rombene.`,
+      ],
+      [
+        `Rwoth Mungu En e jabero, E jakisa, Ene jamer.
+E jakony mwa, E jatel yo, E jalar mwa, Jakwac ma ber.`,
+      ],
+    ],
+  },
+  {
+    number: 2,
+    key: "Eb",
+    tempo: `3/4`,
+    collections: ["GH 149", "SW 48"],
+    titles: ["E mer pa Mungu dit", "The Love of God"],
+    body: [
+      [
+        `E mer pa Mungu dit apila, wacopo pore tak de ngo;
+Ekadhu malu ku wi cero, man cil i tipopiny mi tho.
+Ju ma gisendri dit i dubo, Rwoth Yesu tho pigi de,
+Elwong'ogi gibin i bang'e, Ewek dubo migi pet.`,
+        `E mer pa Mungu dit apila, wacopo pore de ngo.
+Ju ma pwe ceke gibifoyo, mer pare cil nja ku nja.`,
+      ],
+      [
+        `Ker ceke ma ni keni i ng'om, ku lonyone binyothri pet.
+Ju ma gikwero rwo ni Mungu, ndiri bimakugi ma gwei.
+Man mer pa Mungu bijwik de ngo, birem de ngo cil mi nja.
+Bero ma copo bodho dhanu, eromo pak nja ku nja.`,
+        `E mer pa Mungu dit apila, wacopo pore de ngo.
+Ju ma pwe ceke gibifoyo, mer pare cil nja ku nja.`,
+      ],
+      [
+        `Kadok jupong'o ng'om ku polo, ku lembe ma jugoro ngi
+Pi mer pa Mungu yor i bang'wa, m'Enyutho iwa i Wode:
+Jucopo ninyutho tak de ng0, dit mi mer pare bang'wa.
+Kitabu ceke ma jukiewo, binyutho ng'ete kende.`,
+        `E mer pa Mungu dit apila, wacopo pore de ngo.
+Ju ma pwe ceke gibifoyo, mer pare cil nja ku nja.`,
+      ],
+    ],
+  },
+  {
+    number: 3,
+    key: "Ab",
+    tempo: "6/4",
+    collections: ["BA 3", "IH 369", "SW 27"],
+    titles: ["Tel wiwa Mungu", "Majestic Sweetness"],
+    body: [
+      [
+        `Tel wiwa Mungu ku ba ceng', fodi wa n'i ng'om.
+I lembe ma watimo kpo, In m'Itelo wiwa ri, Itelo wiwa ri.`,
+      ],
+      [
+        `Tek wabeyenyo cora swa, i yo ma ber ungo,
+Itirwa. Rwoth i wang'ayo kum 
+wayenyo ngo wakier x2`,
+      ],
+      [
+        `Tek wabewotho kudi ndhu, i tic ku ba rupiny,
+Lembe mwa ceke cungo cu kum
+Itelo wiwa rị x2`,
+      ],
+      [
+        `Ka lem ma tek uwok iwa, de lembe moko mbe,
+Kum lworo negowa ungo, kum
+Itelo wiwa ri x2`,
+      ],
+    ],
+  },
+  {
+    number: 4,
+    key: "E",
+    tempo: "4/4",
+    collections: ["BA 4", "IH 20", "SW 5"],
+    titles: ["Mungu Rwoth ma leng'", "Holy, Holy, Holy"],
+    body: [
+      [
+        `Mungu Rwoth ma leng' kwe, m’I jatego ceke,
+Pir kugweno wer mwa de idho i beng'i tin.
+Mungu Rwoth ma leng' kwe, jatego, jakisa.
+Mungu acel m'unyuthre i adek.`,
+      ],
+      [
+        `Mungu Rwoth ma leng' kwe, ju m'uboth gipeki,
+Gin girwo i beng'i ku the komker m'uberieny.
+Malaika mi polo, de girum i weng'1,
+M'Itie ku kwong'a, tin de, nja ku nja.`,
+      ],
+      [
+        `Mungu Rwoth ma leng' kwe, kadok mudho geng'i
+Kadok ju m'udubo gibeneno dwong'ni ngo.
+In I leng' I kendi, ng'a mange rom kudi?
+Tegoni, merni, leng'ni sagu kpo.`,
+      ],
+      [
+        `Mungu Rwoth ma leng' kwe, m'I jatego ceke,
+Ju mi ng'om ku polo de gipaku nyingi tin.
+Mungu Rwoth ma leng' kwe, jatego, jakisa,
+Mungu acel, m'unyuthre i adek.`,
+      ],
+    ],
+  },
+  {
+    number: 5,
+    key: "G",
+    tempo: "4/4",
+    collections: ["BA 5", "CH 317", "SW 56"],
+    titles: ["Ubimo ma dwong'", "Lord of Glory"],
+    body: [
+      [
+        `Ubimo ma dwong' wapeki, Yesu Kristo Wod Mungu.
+Wan ceke warum i weng'i In i dwong' ma sagu kpo.
+In i kendi m'Iromo, kum tic peri ceke cu.`,
+      ],
+      [
+        `Ubimo ma dwong' wapeki, yang' jukweri, juguri.
+Ento tho Ivoyo ceke, tin In i jacampacu.
+In i kendi m'Iromo, kum Itho pi dhanu yu.`,
+      ],
+      [
+        `Ubimo ma dwong' wapeki, Mungu m'umeri ketho
+Kwong' mi dubo mwa i wii, dwoki lam mi dubo kpo.
+In i kendi m'Iromo, kum Ivoyo tho iwa.`,
+      ],
+      [
+        `Ubimo ma dwong' wapeki, kum Icer kind ju m'utho.
+Kristo pa Mungu wayungi, Ibibimo dhanu yu.
+In i kendi m’Iromo, kum i Rwoth mi rwodhi kpo.`,
+      ],
+    ],
+  },
+  {
+    number: 6,
+    key: "F",
+    tempo: "4/4",
+    collections: ["BA 6", "IH 485", "SW 4"],
+    titles: ["Kwiocwiny pa Mungu", "Like a River Glorious"],
+    body: [
+      [
+        `Kwiocwiny pa Mungu calu pi ma mol,
+Mondo ndhu i cwinywa, kwio racne kpo.
+Ceng' ku ceng' enyai m'emedre iwa cu,
+Man edoko thuth ketho wabedo yo.`,
+        `Mungu mwa Yehova kwio cwinywa ndhu,
+Gin ma dhoge wacu en e lemandha.`,
+      ],
+      [
+        `Wapondo i cinge kum lembe ma rac
+Yenyo nyayu ndiri, pidoic mi ndra.
+Kud ipid lem ng'om ku gin ma galu ngo.
+Bed yo, Mungu kuri, kum Eol ungo.`,
+        `Mungu mwa Yehova kwio cwinywa ndhu,
+Gin ma dhoge wacu en e lemandha.`,
+      ],
+      [
+        `Kud ilwor masendi, Mungu yie En,
+Mer pare bikuri Ebirieme cen.
+Gene nindo ceke, Ebikonyi de,
+Kwiocwiny pa Mungu, kwiocwiny ma dhe.`,
+        `Mungu mwa Yehova kwio cwinywa ndhu,
+Gin ma dhoge wacu en e lemandha.`,
+      ],
+    ],
+  },
+  {
+    number: 7,
+    key: "G",
+    tempo: "3/4",
+    collections: ["BA 7", "IH 21", "SW 6"],
+    titles: ["Wapeki Mungu Wegwa", "Come Thou Almighty King"],
+    body: [
+      [
+        `Wapeki Mungu Wegwa, In i won Lembe mandha.
+Wabepeki! In i Wegwa ma leng' Mungu ma piri dwong',
+Wamio yung iri, Wegwa mi nja.`,
+      ],
+      [
+        `Wapeki Yesu ma Wod, Lembe m'uloro i ng'om.
+Wabepeki! Rombo m'utho ni lam m'Isendri dit ku can,
+Jalar m'ubodhowa, Yesu Kristo.`,
+      ],
+      [
+        `Wapeki Tipo Maleng', Tipo ma jajuk mandha.
+Wabepeki! Itelo wiwa cu, wakwei Iromwa do,
+Man bed japonji mwa, Tipo Maleng'.`,
+      ],
+      [
+        `Wapeki Mungu acel, m' Inyuthrii yo adek.
+Wabepeki! Mungu, Wegwa, ku Wod, Tipo ma juko jo,
+Wapeki ku cwinywa ma nja ku nja.`,
+      ],
+    ],
+  },
+  {
+    number: 8,
+    key: "Eb",
+    tempo: "4/4",
+    collections: ["BA 10", "CH 281", "SW 40"],
+    titles: ["Adieng'acwiny m'utimowa", "How Sweet the Name"],
+    body: [
+      [
+        `Adieng'acwiny m'utimowa wabol i wi Yesu,
+Kum ie pido dit piwa, Ekurowa de du.`,
+      ],
+      [
+        `Ka ter ma pek unurowa, evoyowa mandha,
+Wakwaye, man Ekonyowa, Yehova, tego mwa.`,
+      ],
+      [
+        `Tek Mungu mulo cwinywa de, ku can, ku pidoic,
+Wang'eyo En Eponjowa, wabed ku yioyic.`,
+      ],
+      [
+        `E ka can mwa ma keni thum Edwokowa ndhu cu
+Rwoth mwa bijolowa malu i bang'e i polo.`,
+      ],
+    ],
+  },
+  {
+    number: 9,
+    key: "A",
+    tempo: "3/4",
+    collections: ["BA 11", "IH 407", "SW 30"],
+    titles: ["Ubimo ma Rwoth", "O Worship the King"],
+    body: [
+      [
+        `Ubimo ma Rwoth, m'Edwong' i polo,
+Wer ire pi mer ku tego pare.
+E ban mwa ku pala, asi ku tong' de,
+Dong' wapak nying' Mungu ku tego ceke.`,
+      ],
+      [
+        `Pak bero pare, ku riekone de,
+Derne romo ng'om, elero wang'wa.
+Kum kwinyo m'Edagu ko dubo ma rac,
+Ng'ecwiny pare opo kum lembe ma swa.`,
+      ],
+      [
+        `Ipido piwa, Igwokowa dre,
+I Wegwa mi kwo, m'Ikonyowa le.
+Waneno kony peri i koth ku ceng' de,
+Icego cam iwa, wafoyi awe.`,
+      ],
+      [
+        `Wa dhanu ma ng'ic, jubidhowa dit,
+Lem kum ku lem ng'om, Jok sendowa de.
+Wageni Rwoth Yesu m'Ivoyo mandha,
+Rucwic, ku Jalar mwa, jarimbwa mandha.`,
+      ],
+      [
+        `Mugisa iri kum tegoni lwar,
+ku mer peri de m'Imaru kowa.
+Wameri, wadodi wapeki Mungu!
+Jole wer mwa ku pak ma wapeki kudo`,
+      ],
+    ],
+  },
+  {
+    number: 10,
+    key: "D",
+    tempo: "4/4",
+    collections: ["IH 116", "SW 24"],
+    titles: ["Con Yesu lor'i ng'om", "What a Wonderful Saviour"],
+    body: [
+      [
+        `Con Yesu lor'i ng'om mae, Yesu ma Jalar mwa.
+Eweko lonyo ma pare, Yesu ma Jalar mwa.`,
+        `Yesu ma Jalar mwa, wa dhanu wameri.
+Yesu ma Jalar mwa, warwo i beng'i.`,
+      ],
+      [
+        `Esendre ya Elar kowa, Yesu ma Jalar mwa.
+Ekwero ngo nitho kumwa, Yesu ma Jalar mwa.`,
+      ],
+      [
+        `Ka Yesu tho Eai k'i liel, Yesu ma Jalar mwa.
+Elund Eidho ka maleng', Yesu ma Jalar mwa.`,
+      ],
+      [
+        `Ka Yesu ai kud i polo, Yesu ma Jalar mwa.
+Bidwogo kar' Ecokwa kpo, Yesu ma Jalar mwa.`,
+      ],
+    ],
+  },
+  {
+    number: 11,
+    key: "G",
+    tempo: "4/4",
+    collections: ["BA 13", "IH 237", "SW 7"],
+    titles: ["Pak nying Rwoth", "All Hail the Power (Coronation)"],
+    body: [
+      [
+        `Pak nying Rwoth Yesu, nying ma dwong',
+Malaika rum ire.
+Jamkermiwic waroi wie, Wayunge Rwoth mi rwodhi! x2`,
+      ],
+      [
+        `Wu piny pare ma piwu tek i wang Jawar ma Rwoth.
+Ekoyowu pi tic ma pwe, wuyunge Rwoth mi rwodhi! x2`,
+      ],
+      [
+        `Wu suru ceke, dhanu yu, m'ubedo kwo i ng'om.
+Wudode ni ubimo mu, wuyunge Rwoth mi rwodhi! x2`,
+      ],
+      [
+        `Ka wabitundo kuca ma wajwigra the tiendi,
+Wapeki dit ku wer ma nja, Wayungi Rwoth mi rwodhi! x2`,
+      ],
+    ],
+  },
+];
